c3bfb98523904324a8362913e27e307b21ea74e4
Fernando Lucas <fernandolcsdev@outlook.com>
2023-03-16 14:31:29 -0300

    Configurado Router

---
+++ src/components/layout/Container.js
@@ -0,0 +1,8 @@
+import styles from './Container.module.css'
+
+function Container(props) {
+    console.log(styles)
+    return(<div className={`${styles.container} ${styles[props.customClass]}`}>{props.children}</div>)
+}
+
+export default Container
